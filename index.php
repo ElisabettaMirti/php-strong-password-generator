e50d331d82e30e533e4dca97f47e084235f58deb
ElisabettaMirti <belmirti@gmail.com>
2024-07-02 16:54:46 +0200

    creato array di utenti con password

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$users = [
+    [
+        "user" => "Edmond",
+        "password" => "Montecristo"
+    ],
+    [
+        "user" => "Jekyll",
+        "password" => "MrHyde"
+    ],
+    [
+        "user" => "Beatrix",
+        "password" => "Bride"
+    ],
+    [
+        "user" => "Michelangelo",
+        "password" => "Caravaggio"
+    ],
+    [
+        "user" => "Shinji",
+        "password" => "Evangelion"
+    ],
+];
